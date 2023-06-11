7cee34ee9213181796252f9880680484f709c08b
wil-mus <wilfred_musyoka@yahoo.com>
2023-06-11 12:26:11 -0700

    fixed betty

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    
+
     multiples = []
     for i in range(len(my_list)):
         if my_list[i] % 2 == 0:
--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    
+
     a = tuple_a[:2]
     b = tuple_b[:2]
 
