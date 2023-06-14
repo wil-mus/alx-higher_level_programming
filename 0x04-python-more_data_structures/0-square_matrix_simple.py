cdcce811c3e1ecaffc08fbdec5c0f7b1dea70a6e
wil-mus <wilfred_musyoka@yahoo.com>
2023-06-14 11:08:46 -0700

    matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    results = []
+    for row in matrix:
+        new_row = []
+        for num in row:
+            new_row.append(num ** 2)
+            result.append(new_row)
+        return result
