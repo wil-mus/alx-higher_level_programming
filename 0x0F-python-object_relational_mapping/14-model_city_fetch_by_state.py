9e53182b71477cb1a38899df267a26b6745bb499
wil-mus <wilfred_musyoka@yahoo.com>
2023-09-16 18:42:44 -0700

    initial change

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,41 +1,22 @@
--- Create database hbtn_0e_14_usa, tables states and cities + some data
-CREATE DATABASE IF NOT EXISTS hbtn_0e_14_usa;
-USE hbtn_0e_14_usa;
-CREATE TABLE IF NOT EXISTS states (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id)
-);
-INSERT INTO states (name)
-VALUES ("California"),
-    ("Arizona"),
-    ("Texas"),
-    ("New York"),
-    ("Nevada");
-CREATE TABLE IF NOT EXISTS cities (
-    id INT NOT NULL AUTO_INCREMENT,
-    state_id INT NOT NULL,
-    name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY(state_id) REFERENCES states(id)
-);
-INSERT INTO cities (state_id, name)
-VALUES (1, "San Francisco"),
-    (1, "San Jose"),
-    (1, "Los Angeles"),
-    (1, "Fremont"),
-    (1, "Livermore");
-INSERT INTO cities (state_id, name)
-VALUES (2, "Page"),
-    (2, "Phoenix");
-INSERT INTO cities (state_id, name)
-VALUES (3, "Dallas"),
-    (3, "Houston"),
-    (3, "Austin");
-INSERT INTO cities (state_id, name)
-VALUES (4, "New York");
-INSERT INTO cities (state_id, name)
-VALUES (5, "Las Vegas"),
-    (5, "Reno"),
-    (5, "Henderson"),
-    (5, "Carson City");
+#!/usr/bin/python3
+"""
+prints all City objects from the database hbtn_0e_14_usa
+"""
+
+import sys
+from venv import create
+from sqlalchemy import create_engine, true
+from sqlalchemy.orm import sessionmaker
+from model_state import State
+from model_city import City
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    session_maker = sessionmaker(bind=engine)
+    session = session_maker()
+
+    for city, state in session.query(City, State)\
+        .filter(City.state_id == State.id)\
+            .order_by(City.id):
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
